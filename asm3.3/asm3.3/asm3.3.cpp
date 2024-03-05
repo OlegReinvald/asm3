b8e23fe3698862a65d6e0560fd568b1ce73974f3
Oleg <olejatbwp@gmail.com>
2024-03-05 19:35:52 +0300

    .

--- asm3.3/asm3.3/asm3.3.cpp
+++ asm3.3/asm3.3/asm3.3.cpp
@@ -1,25 +1,24 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int series[48];
+    int series[47];
     series[0] = 0;
     series[1] = 1;
     _asm {
-        mov ecx, 2
-        mov eax, 0
+        mov esi, 8
+        mov ecx, 0
         mov ebx, 1
         iter:
-        mov edx, 0
-        add edx, eax
-        add edx, ebx
-        jo exit
-        mov series[ecx*4], edx
-        inc ecx
-        mov eax, ebx
-        mov ebx, edx
-        jmp iter
-        exit:
- 
+        mov eax, ecx
+            add eax, ebx
+            jo exit1
+            mov series[esi], eax
+            add esi, 4
+            mov ecx, ebx
+            mov ebx, eax
+            jmp iter
+            exit1 :
+
     }
     for (int i = 0; i < 47; i++) {
         cout << series[i] << " ";
